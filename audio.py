9475af1e2c61f3379c0e9c195a07761a047c5b56
tameemalaa <tameem.alaa@gmail.com>
2023-01-11 06:15:34 +0200

    Added Audio Class based on Thread class

---
+++ audio.py
@@ -0,0 +1,16 @@
+import os
+from threading import Thread
+from playsound import playsound
+from constants import AUDIO_DIRECTORY
+
+
+PARENT_DIR = os.path.dirname(os.path.abspath(__file__)).replace(":", ":\\")
+
+
+class Audio(Thread):
+    def __init__(self, audio_file: str) -> None:
+        Thread.__init__(self)
+        self.audio = os.path.join(PARENT_DIR, AUDIO_DIRECTORY, audio_file)
+
+    def run(self):
+        playsound(self.audio)
